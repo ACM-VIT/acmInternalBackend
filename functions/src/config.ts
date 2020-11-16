5353043cef62b99893610f05202c84d061d5e7ae
madrigal1 <kurupgokul11@gmail.com>
2020-11-16 21:19:47 +0530

    fix: changed andorid client id

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -7,5 +7,5 @@ export const tokenInfo = {
     audience:"acmvit.in",
 }
 export const googleClientId_reactnative = "334320953247-hlvp0dom4i7ifhqtl0mb2mpq6phir509.apps.googleusercontent.com";
-export const googleClientId_android = "334320953247-4dbac0bjq2dkbge97c9sndfc0lhmbusc.apps.googleusercontent.com";
+export const googleClientId_android = "334320953247-cebtvillamn4dv3cgihn35kilt9sb6nc.apps.googleusercontent.com";
 
